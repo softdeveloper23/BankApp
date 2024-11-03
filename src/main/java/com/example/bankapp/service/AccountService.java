dd67c34cb721bf94bb59e117b7f2ce67d8412dc1
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-03 19:55:34 +0100

    Added code to AccountService class

--- src/main/java/com/example/bankapp/service/AccountService.java
+++ src/main/java/com/example/bankapp/service/AccountService.java
@@ -1,7 +1,37 @@
 package com.example.bankapp.service;
 
+import com.example.bankapp.model.Account;
+import com.example.bankapp.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account findAccountByUsername(String username) {
+        return accountRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Account not found!"));
+    }
+
+    public Account registerAccount(String username, String password) {
+        if (accountRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("Username already exists!");
+        }
+
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(passwordEncoder.encode(password));
+        account.setBalance(BigDecimal.ZERO);
+        return accountRepository.save(account);
+    }
 }
