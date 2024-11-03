d6ec08501371c1f273e839aac16ec08a773643f7
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-03 15:57:05 +0100

    Added private property Account to Transaction class

--- src/main/java/com/example/bankapp/model/Transaction.java
+++ src/main/java/com/example/bankapp/model/Transaction.java
@@ -1,6 +1,6 @@
 package com.example.bankapp.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -8,8 +8,14 @@ import java.time.LocalDateTime;
 @Entity
 public class Transaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal amount;
     private String type;
     private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
 }
