fce32f163f341ec701a96bcc3ae6c0a0c608a373
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-03 15:58:38 +0100

    Added constructors and getter/setter in Transaction class

--- src/main/java/com/example/bankapp/model/Transaction.java
+++ src/main/java/com/example/bankapp/model/Transaction.java
@@ -18,4 +18,54 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "account_id")
     private Account account;
+
+    public Transaction() {
+    }
+
+    public Transaction(BigDecimal amount, String type, LocalDateTime timestamp, Account account) {
+        this.amount = amount;
+        this.type = type;
+        this.timestamp = timestamp;
+        this.account = account;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
