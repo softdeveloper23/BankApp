be95343887e0ef3963c19a8423badf16136e7163
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-03 15:35:50 +0100

    Added Account class and properties

---
+++ src/main/java/com/example/bankapp/model/Account.java
@@ -0,0 +1,18 @@
+package com.example.bankapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private BigDecimal balance;
+}
