9f3660e7c1b7896282110b8baaeb59d80c91cca1
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-03 19:40:55 +0100

    Added code to AccountRepository

--- src/main/java/com/example/bankapp/repository/AccountRepository.java
+++ src/main/java/com/example/bankapp/repository/AccountRepository.java
@@ -1,7 +1,10 @@
 package com.example.bankapp.repository;
 
+import com.example.bankapp.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+import java.util.Optional;
 
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
 }
