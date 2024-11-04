f0c3f550c8e050b309d5b8c5b6842d953fe32a64
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-04 14:09:28 +0100

    Updated Account class adding implements UserDetails to class

--- src/main/java/com/example/bankapp/model/Account.java
+++ src/main/java/com/example/bankapp/model/Account.java
@@ -2,13 +2,14 @@ package com.example.bankapp.model;
 
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class Account {
+public class Account implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
