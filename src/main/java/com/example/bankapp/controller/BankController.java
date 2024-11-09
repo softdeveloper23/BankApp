ac84c8fcd927d42d9844fabdc03bb42deb748111
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-09 19:15:43 +0100

    Added BankController class

--- src/main/java/com/example/bankapp/controller/BankController.java
+++ src/main/java/com/example/bankapp/controller/BankController.java
@@ -1,12 +1,23 @@
 package com.example.bankapp.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.example.bankapp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BankController {
 
     @Autowired
     private AccountService accountService;
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account account = accountService.findAccountByUsername(username);
+        model.addAttribute("account", account);
+        return "dashboard";
+    }
 }
